108291c3caba0b52b079f1b5c66af7c170ee4a14
prakashsapkotanp <126087629+prakashsapkotanp@users.noreply.github.com>
2023-10-31 21:43:34 +0545

    initial commit
    commit project skeleton

---
+++ bloodlinkbackendapi/src/main/java/bloodllink/app/bloodlinkbackendapi/BloodlinkbackendapiApplication.java
@@ -0,0 +1,13 @@
+package bloodllink.app.bloodlinkbackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlinkbackendapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodlinkbackendapiApplication.class, args);
+	}
+
+}
---
+++ bloodlinkbackendapi/src/test/java/bloodllink/app/bloodlinkbackendapi/BloodlinkbackendapiApplicationTests.java
@@ -0,0 +1,13 @@
+package bloodllink.app.bloodlinkbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodlinkbackendapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
